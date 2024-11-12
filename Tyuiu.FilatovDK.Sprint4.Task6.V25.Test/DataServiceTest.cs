2074412d9f034e2530b651e3dec7deafc860bca9
kiki769 <fiirve@bk.ru>
2024-11-13 00:25:34 +0500

    Task6

---
+++ Tyuiu.FilatovDK.Sprint4.Task6.V25.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.FilatovDK.Sprint4.Task6.V25.Lib;
+namespace Tyuiu.FilatovDK.Sprint4.Task6.V25.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var ok = new string[] { "Теннис", "Футбол", "Крикет", "Баскетбол", "Бейсбол", "Регби", "Хоккей" };
+            int res = ds.Calculate(ok);
+            int wait = 1;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.FilatovDK.Sprint4.Task6.V25/Program.cs
@@ -0,0 +1,49 @@
+﻿using Tyuiu.FilatovDK.Sprint4.Task6.V25.Lib;
+namespace Tyuiu.FilatovDK.Sprint4.Task6.V25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataService ds = new DataService();
+
+
+            Console.Title = "Спринт #4 | Выполнил: Филатов Д.К. │ СМАРТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Класс Array                                                       *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #25                                                             *");
+            Console.WriteLine("* Выполнил: Филатов Денис Константинович  | СМАРТб-24-1                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан строковый массив данных, используя класс Array подсчитайте          *");
+            Console.WriteLine("* количество элементов, длина которых больше 6.                           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string[] array = new string[] { "Теннис", "Футбол", "Крикет", "Баскетбол", "Бейсбол", "Регби", "Хоккей" };
+            Console.WriteLine("Исходный массив:");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Количество элементов, длина которых больше 6:");
+            int res = ds.Calculate(array);
+
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+
+        }
+    }
+}
